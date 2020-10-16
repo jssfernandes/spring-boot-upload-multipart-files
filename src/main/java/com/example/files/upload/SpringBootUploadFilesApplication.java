77689ae38edff9d7a9dde6ca0d5cf1e912de5237
Josenilton Fernandes <josenilton.fernandes@original.com.br>
2020-10-16 00:21:45 -0300

    commit inicial

---
+++ src/main/java/com/example/files/upload/SpringBootUploadFilesApplication.java
@@ -0,0 +1,24 @@
+package com.example.files.upload;
+
+import com.example.files.upload.service.FilesStorageService;
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUploadFilesApplication implements CommandLineRunner {
+  @Resource
+  FilesStorageService storageService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+  }
+
+  @Override
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }
+}
